8a82107e057e2f8d29b8388be8534b69812eaf3d
Kiran Shila <me@kiranshila.com>
2022-08-18 15:09:26 -0700

    add all the CI

--- siglent/common.py
+++ siglent/common.py
@@ -1,8 +1,12 @@
+"""Commom utilities for working with all siglent GPIB devices."""
+
 from pyvisa import ResourceManager
 from pyvisa.resources import MessageBasedResource
 
 
 class MessageResource:
+    """Base class for all message-based siglent gpib resources."""
+
     def __init__(self, visa_address: str, rm: ResourceManager):
         res = rm.open_resource(visa_address)
         if isinstance(res, MessageBasedResource):
@@ -12,22 +16,19 @@ class MessageResource:
 
     @property
     def identifier(self) -> str:
-        """Returns a unique identifier of the deivce"""
+        """Get the unique identifier of the deivce."""
         return self._resource.query("*IDN?")
 
     def reset(self):
-        """
-        This command presets the instrument to a factory defined condition that is appropriate for
-        remote programming operation.
-        """
+        """Reset the instrument to a factory defined condition."""
         self._resource.write("*RST")
 
     def clear(self):
-        """Clear the instrument status byte by emptying the error queue and clearing all event registers"""
+        """Clear the instrument status byte."""
         self._resource.write("*CLS")
 
     def block_until_complete(self):
-        """Blocks the runtime until the instrument has finished all prior operations"""
+        """Block the runtime until the instrument has finished all prior operations."""
         assert (
             self._resource.query("*OPC?").strip() == "1"
         ), "*OPC? returned something unexpected"
--- siglent/spectrum_analyzers.py
+++ siglent/spectrum_analyzers.py
@@ -1,13 +1,29 @@
+"""Spectrum analyzers.
+
+# SSA3000X
+Supports:
+- SSA3000X Plus
+
+and
+
+- SSA3000X-R
+- SVA1000X
+
+in Spectrum Analyzer mode.
+
+"""
+
 from enum import Enum
-from .common import MessageResource
 from typing import List
+
+from .common import MessageResource
 
 # Fixed for every SA in this series
 NUM_DATA_POINTS = 751
 
 
 class Bandwidth(Enum):
-    """The discrete bandwidth values for Resolution and Video Bandwidth"""
+    """The discrete bandwidth values for Resolution and Video Bandwidth."""
 
     HZ_1 = 1
     HZ_3 = 3
@@ -25,12 +41,25 @@ class Bandwidth(Enum):
 
 
 class AverageType(Enum):
+    """Trace averaging type."""
+
     LOG_POWER = "LOGP"
     POWER = "POW"
     VOLTAGE = "VOLT"
 
 
 class TraceMode(Enum):
+    """Trace display mode.
+
+    Values
+    ------
+    * WRITE - Continuously update
+    * MAX_HOLD - Store the maximum value of each point
+    * MIN_HOLD - Store the minimum value of each point
+    * VIEW - Display the last recorded point, don't update
+    * AVERAGE - Turn on trace averaging
+    """
+
     WRITE = "WRIT"
     MAX_HOLD = "MAXH"
     MIN_HOLD = "MINH"
@@ -39,6 +68,31 @@ class TraceMode(Enum):
 
 
 class DetectionMode(Enum):
+    """Trace detection mode.
+
+    Values
+    ------
+    * NEGATIVE - Negative peak detection displays the lowest sample taken during
+    the interval being displayed.
+    * POSITIVE - Positive peak detection displays the highest sample taken during
+    the interval being displayed.
+    * SAMPLE - Sample detection displays the sample taken during the interval being
+    displayed, and is used primarily to display noise or noise-like signals.
+    In sample mode, the instantaneous signal value at the present display point is
+    placed into memory. This detection should not be used to make the most accurate
+    amplitude measurement of non noise-like signals.
+    * AVERAGE - Average detection is used when measuring the average value of the
+    amplitude across each trace interval (bucket). The averaging method used by the
+    average detector is set to either video or power as appropriate when the average
+    type is auto coupled.
+    * NORMAL - Normal detection selects the maximum and minimum video signal values
+    alternately. When selecting Normal detection,”Norm”appears in the upper-left corner.
+    * QUASI - Quasipeak detection is a form of detection where a signal level is
+    weighted based on the repetition frequency of the spectral components making up the
+    signal. That is to say, the result of a quasi-peak measurement depends on the
+    repetition rate of the signal.
+    """
+
     NEGATIVE = "NEG"
     POSITIVE = "POS"
     SAMPLE = "SAMP"
@@ -48,16 +102,18 @@ class DetectionMode(Enum):
 
 
 class SSA3000X(MessageResource):
+    """The class to control a SSA3000X-Series spectrum analyzer."""
+
     @property
     def ref_level(self) -> float:
-        """Gets the current reference level in dBm"""
-        return float(self._resource.query(f":DISP:WIND:TRAC:Y:RLEV?"))
+        """Get the reference level in dBm."""
+        return float(self._resource.query(":DISP:WIND:TRAC:Y:RLEV?"))
 
     # ----- Display -----
 
     @ref_level.setter
     def ref_level(self, level_dbm: float):
-        """Sets the reference level to the specified value in dBm"""
+        """Set the reference level to the specified value in dBm."""
         assert -100 <= level_dbm <= 30, "Level must be between -100 and 30 dBm"
         self._resource.write(f":DISP:WIND:TRAC:Y:RLEV {level_dbm} DBM")
 
@@ -65,12 +121,12 @@ class SSA3000X(MessageResource):
 
     @property
     def span(self) -> float:
-        """Gets the current span in Hz"""
+        """Get the current span in Hz."""
         return float(self._resource.query(":FREQ:SPAN?"))
 
     @span.setter
     def span(self, freq_hz: float):
-        """Sets the span in Hz"""
+        """Set the span in Hz."""
         assert (100 <= freq_hz <= 3.2e9) or (
             freq_hz == 0
         ), "Span must be between 100 Hz and 3.2 GHz or 0"
@@ -78,116 +134,117 @@ class SSA3000X(MessageResource):
 
     @property
     def freq_center(self) -> float:
-        """Gets the center frequency in Hz"""
+        """Get the center frequency in Hz."""
         return float(self._resource.query(":FREQ:CENT?"))
 
     @freq_center.setter
     def freq_center(self, freq_hz: float):
-        """Sets the center frequency in Hz"""
+        """Set the center frequency in Hz."""
         assert 0 <= freq_hz <= 3.2e9, "Center frequency must be between 0 and 3.2 GHz"
         self._resource.write(f":FREQ:CENT {freq_hz} Hz")
 
     @property
     def freq_start(self) -> float:
-        """Gets the start frequency in Hz"""
+        """Get the start frequency in Hz."""
         return float(self._resource.query(":FREQ:STAR?"))
 
     @property
     def freq_stop(self) -> float:
-        """Gets the stop frequency in Hz"""
+        """Get the stop frequency in Hz."""
         return float(self._resource.query(":FREQ:STOP?"))
 
     @freq_stop.setter
     def freq_stop(self, freq_hz: float):
-        """Sets the stop frequency in Hz"""
+        """Set the stop frequency in Hz."""
         assert 0 <= freq_hz <= 3.2e9, "Stop frequency must be between 0 and 3.2 GHz"
         self._resource.write(f":FREQ:STOP {freq_hz} Hz")
 
     @property
     def freq_step(self) -> float:
-        """Gets the current frequency step size (controlled by span)"""
+        """Get the current frequency step size (controlled by span)."""
         return float(self._resource.query(":FREQ:CENT:STEP?"))
 
     # ----- Power -----
 
     @property
     def attenuation(self) -> float:
-        """Gets the current attenuation level in dB"""
+        """Get the current attenuation level in dB."""
         return float(self._resource.query(":POW:ATT?"))
 
     @attenuation.setter
     def attenuation(self, db: float):
-        """Sets the attenuation level between 0 and 51 dB"""
+        """Set the attenuation level between 0 and 51 dB."""
         assert 0 <= db <= 51, "Attenuation must be between 0 and 51 dB"
         self._resource.write(f":POW:ATT {db}")
 
     @property
     def preamp(self) -> bool:
-        """Gets the status of the internal preamp. Returns true if active"""
+        """Get the status of the internal preamp. Returns true if active."""
         return self._resource.query("POW:GAIN?").strip() == "1"
 
     @preamp.setter
     def preamp(self, enabled: bool):
-        """Sets the internal preamp state to `enabled`"""
+        """Set the internal preamp state to `enabled`."""
         self._resource.write(f":POW:GAIN {'ON' if enabled else 'OFF'}")
 
     # ----- Bandwidth -----
 
     @property
     def rbw(self) -> Bandwidth:
-        """Gets the resolution bandwidth"""
+        """Get the resolution bandwidth."""
         return Bandwidth(float(self._resource.query(":BWID?")))
 
     @rbw.setter
     def rbw(self, bw: Bandwidth):
-        """Sets the resolution bandwidth"""
+        """Set the resolution bandwidth."""
         self._resource.write(f":BWID {bw.value}")
 
     @property
     def vbw(self) -> Bandwidth:
-        """Gets the video bandwidth"""
+        """Get the video bandwidth."""
         return Bandwidth(float(self._resource.query(":BWID:VID?")))
 
     @vbw.setter
     def vbw(self, bw: Bandwidth):
-        """Sets the video bandwidth"""
+        """Set the video bandwidth."""
         self._resource.write(f":BWID:VID {bw.value}")
 
     @property
     def average_type(self) -> AverageType:
-        """Gets the average type"""
+        """Get the average type."""
         return AverageType(self._resource.query(":AVER:TYPE?").strip())
 
     @average_type.setter
     def average_type(self, type: AverageType):
-        """Sets the average type"""
+        """Set the average type."""
         self._resource.write(f":AVER:TYPE {type.value}")
 
     # ----- Trace -----
 
     @property
     def sweep_time(self) -> float:
-        """Gets the current sweep time in seconds"""
+        """Get the current sweep time in seconds."""
         return float(self._resource.query(":SWE:TIME?"))
 
     @sweep_time.setter
     def sweep_time(self, time: float):
-        """Sets the sweep time to a value from 450us to 1.5ks"""
+        """Set the sweep time to a value from 450us to 1.5ks."""
         assert 450e-6 <= time <= 1.5e3, "Time must be between 450us and 1.5ks"
         self._resource.write(f":SWE:TIME {time}s")
 
     def sweep_restart(self):
-        """Retsarts the current sweep"""
+        """Retsart the current sweep."""
         self._resource.write(":INIT:REST")
 
     def trace(self, trace: int) -> "Trace":
-        """Gets the trace object from the SA"""
+        """Get the trace object from the SA."""
         assert 1 <= trace <= 4, "Trace is either 1,2,3, or 4"
         return self.Trace(self, trace)
 
     class Trace:
+        """The trace object for one of the four available traces."""
+
         def __init__(self, parent: "SSA3000X", n: int):
-            """Constructor for a trace object, don't call directly, use .trace()"""
             self._n = n
             self._parent = parent
             self._resource = parent._resource
@@ -196,56 +253,58 @@ class SSA3000X(MessageResource):
 
         @property
         def mode(self) -> TraceMode:
-            """Gets the current trace mode"""
+            """Get the current trace mode."""
             return TraceMode(self._resource.query(f":TRAC{self._n}:MODE?").strip())
 
         @mode.setter
         def mode(self, mode: TraceMode):
-            """Sets the trace mode"""
+            """Set the trace mode."""
             self._resource.write(f":TRAC{self._n}:MODE {mode.value}")
 
         @property
         def averages(self) -> int:
-            """Gets the number of averages"""
+            """Get the number of averages."""
             return int(self._resource.query(f":AVER:TRAC{self._n}:COUN?"))
 
         @averages.setter
         def averages(self, n: int):
-            """Sets the number of averages (Between 1 and 999)"""
+            """Set the number of averages (Between 1 and 999)."""
             assert 1 <= n <= 999, "n must be between 1 and 999"
             self._resource.write(f":AVER:TRAC{self._n}:COUN {n}")
 
         @property
         def current_averages(self) -> int:
-            """Gets the current number of averages"""
+            """Get the current number of averages."""
             return int(self._resource.query(f":AVER:TRAC{self._n}?"))
 
         def average_restart(self):
-            """Restarts trace averaging"""
+            """Restart trace averaging."""
             self._resource.write(f":AVER:TRAC{self._n}:CLE")
 
         @property
         def detection_mode(self) -> DetectionMode:
-            """Gets the current trace detection mode"""
+            """Get the current trace detection mode."""
             return DetectionMode(self._resource.query(f":DET:TRAC{self._n}?").strip())
 
         @detection_mode.setter
         def detection_mode(self, mode: DetectionMode):
-            """Sets the trace detection mode"""
+            """Set the trace detection mode."""
             self.resource.write(f":DET:TRAC{self._n} {mode.value}")
 
         @property
         def data(self) -> List[float]:
             """
-            Gets a list of the data point of the *currently displayed* trace.
+            Get a list of the data point of the *currently displayed* trace.
+
             The units of this data is dependent of the current configuration.
             This will force a retrigger of the measurement and wait the sweep time
             before returning a result.
             """
             self._parent.sweep_restart()
             self._parent.block_until_complete()
-            # Siglent seems to be sending little endian, this is mysteriously undocumented
-            # Additionally, there is no header and we know the number of points as this is fixed
+            # Siglent seems to be sending little endian, this is mysteriously
+            # undocumented. Additionally, there is no header and we know
+            # the number of points as this is fixed
             return self._resource.query_binary_values(
                 f"TRAC:DATA? {self._n}",
                 datatype="d",
